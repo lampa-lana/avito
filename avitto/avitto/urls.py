1b65236685cbc14ff7bfcf0492afaaa8d23fd2d3
Svetlana <lampa1979@mail.ru>
2021-09-29 09:25:59 +0400

    change URLS

--- avitto/avitto/urls.py
+++ avitto/avitto/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf.urls import url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'admin/', admin.site.urls),
     path('core/', include('core.urls')),
 ]
--- avitto/core/urls.py
+++ avitto/core/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 from django.conf.urls import url
-
+from django.urls import path, include
 from .views import index, index2
 
 urlpatterns = [
     url('1/', index, name='index'),
-    url('2/', index2, name='index2'),
+    path('2/', index2, name='index2'),
 ]
