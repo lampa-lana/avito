1d42631d3d14de008ae05022aadcb5f9fa6d0ae9
Svetlana <lampa1979@mail.ru>
2021-10-01 09:05:29 +0400

    fix homework02

--- avitto/core/models.py
+++ avitto/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 
@@ -23,24 +24,29 @@ class Profile(models.Model):
         return str(self.user.username)
 
 
-class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    post_name = models.CharField(max_length=30)
-    description = models.TextField(max_length=1000, blank=True)
-    image = models.ImageField(upload_to=user_directory_path)
-    date_pub = models.DateTimeField(auto_now_add=True)
-    date_edit = models.DateTimeField(auto_now=True)
-    price = models.IntegerField()
-    category = models.ManyToManyField(User,  related_name='category')
-
-    def __str__(self) -> str:
-        return '{}, publication date {}, {}, {}'.format(self.author.username, self.date_pub, self.post_name, self.price)
-
-
 class Category(models.Model):
-    # category = models.ManyToManyField(User, Post, related_name='category_product')
     category_name = models.CharField(max_length=30)
-    description = models.TextField(max_length=100, blank=True)
+    description = models.TextField(max_length=1000, blank=True)
 
     def __str__(self) -> str:
         return '{}, {}'.format(self.category_name, self.description)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='user')
+    post_name = models.CharField(max_length=30)
+    description = models.TextField(max_length=1000, blank=True)
+    image = models.ImageField(upload_to=user_directory_path)
+    date_pub = models.DateTimeField(default=timezone.now)
+    date_edit = models.DateTimeField(default=timezone.now)
+    price = models.IntegerField()
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, null=True,  related_name='category')
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self) -> str:
+        return '{}, publication date {}, {}, {}'.format(self.author.username, self.date_pub, self.post_name, self.price)
