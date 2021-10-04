159d664836f108691b6509e1e9672cfbbe495f70
Svetlana <lampa1979@mail.ru>
2021-10-04 13:01:42 +0400

    fix homework02

--- avitto/core/admin.py
+++ avitto/core/admin.py
@@ -8,18 +8,21 @@ class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'id', 'foto', 'birth_date')
     list_display_links = ('user', 'id',)
     search_fields = ('id', 'birth_date')
+    list_filter = ('user', 'id', 'birth_date')
 
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('author', 'post_name', 'date_pub', 'price', 'category')
     list_display_links = ('author', 'post_name', 'category')
-    search_fields = ('post_name', )
+    search_fields = ('post_name', 'author', )
+    list_filter = ('category', 'author',  'date_pub', 'price', )
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('category_name', 'description')
-    list_display_links = ('category_name',)
+    list_display = ('category_name', 'description', )
+    list_display_links = ('category_name', 'description', )
     search_fields = ('category_name',)
+    list_filter = ('author', 'post', 'category_name')
 
 
 admin.site.register(Profile, ProfileAdmin)
