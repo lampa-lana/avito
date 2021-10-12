4f19a43c8fe137c3a181a54d58e6c116593fdaf6
Svetlana <lampa1979@mail.ru>
2021-10-12 16:02:21 +0400

    create homework05

--- avitto/core/urls.py
+++ avitto/core/urls.py
@@ -1,16 +1,21 @@
 from django.conf.urls import url
-from django.conf.urls import url
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 from .views import (post_detail, category_detail,
-                    category_all, post_create, IndexView, AllPostView)
+                    IndexView, AllPostView, PostCreateView, AllCategoryView, PostDelete, EditView)
 
 app_name = 'core'
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
     path('posts/<int:post_id>/', post_detail, name='post_detail'),
-    path('posts/create/', post_create, name='post_create'),
+    path('posts/create/', PostCreateView.as_view(), name='post_create'),
     path('posts/all_posts/', AllPostView.as_view(), name='all_posts'),
-    path('category/', category_all, name='category_all'),
+    path('posts/<int:post_id>/delete/', PostDelete.as_view(), name='post_delete'),
+    path('posts/<int:post_id>/delete-success/',
+         TemplateView.as_view(template_name='core/delete_success.html'), name='post_delete_success'),
+    path('posts/<int:post_id>/edit/', EditView.as_view(), name='post_edit'),
+    path('category/', AllCategoryView.as_view(), name='category_all'),
     path('category/<int:category_id>/', category_detail, name='category_detail'),
 ]
--- avitto/core/views.py
+++ avitto/core/views.py
@@ -2,7 +2,10 @@ from django.db.models.aggregates import Sum
 from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from django.urls import reverse
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post, Category, Profile
 from .forms import PostForm
 
@@ -13,7 +16,6 @@ class IndexView(ListView):
     model = Post
     template_name = 'core/index.html'
     context_object_name = 'posts'
-    categories = Category.objects.all()
 
     def get_queryset(self):
         return self.model.objects.all().order_by('-date_edit')[:7]
@@ -55,13 +57,13 @@ def post_detail(request, post_id):
     return render(request, template_name='core/post_detail.html', context=context)
 
 
-def post_create(request):
-    if request.method == 'GET':
-        form = PostForm()
-        return render(request, 'core/post_create.html', {
-            'form': form})
-    elif request.method == "POST":
-        form = PostForm(request.POST, request.FILES)
+class PostCreateView(LoginRequiredMixin, CreateView):
+    form_class = PostForm
+    template_name = 'core/post_create.html'
+    login_url = '/admin/login'
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
@@ -70,7 +72,44 @@ def post_create(request):
         else:
             return render(request, 'core/post_create.html', {
                 'form': form})
+
+
+# def post_create(request):
+#     if request.method == 'GET':
+#         form = PostForm()
+#         return render(request, 'core/post_create.html', {
+#             'form': form})
+#     elif request.method == "POST":
+#         form = PostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.save()
+#             return redirect(reverse('core:post_detail', kwargs={'post_id': post.id}))
+#         else:
+#             return render(request, 'core/post_create.html', {
+#                 'form': form})
         # return HttpResponse('post_create')
+
+class PostDelete(DeleteView):
+    model = Post
+    pk_url_kwarg = 'post_id'
+    template_name = 'core/post_delete.html'
+
+    def get_success_url(self):
+        post_id = self.kwargs['post_id']
+        return reverse('core:post_delete_success', args=(post_id, ))
+
+
+class EditView(UpdateView):
+    model = Post
+    pk_url_kwarg = 'post_id'
+    template_name = 'core/post_edit.html'
+    form_class = PostForm
+
+    def get_success_url(self):
+        post_id = self.kwargs['post_id']
+        return reverse('core:post_detail', args=(post_id, ))
 
 
 def category_detail(request, category_id):
@@ -85,9 +124,18 @@ def category_detail(request, category_id):
     return render(request, template_name='core/cat_detail.html', context=context)
 
 
-def category_all(request):
-    categories = Category.objects.all()
-    context = {
-        'categories': categories,
-        'title': "Категории", }
-    return render(request, template_name='core/categories.html', context=context)
+class AllCategoryView(ListView):
+    model = Category
+    template_name = 'core/categories.html'
+    context_object_name = 'categories'
+
+    def get_queryset(self):
+        return self.model.objects.all()
+
+
+# def category_all(request):
+#     categories = Category.objects.all()
+#     context = {
+#         'categories': categories,
+#         'title': "Категории", }
+#     return render(request, template_name='core/categories.html', context=context)
