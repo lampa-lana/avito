de277bbb368ea781d2bf03408df71b317193ac01
Svetlana <lampa1979@mail.ru>
2021-10-24 13:45:19 +0400

    fix

--- avitto/avitto/settings.py
+++ avitto/avitto/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#av=zre4o*_s&8i%-kcgu%w-k+qx)a(cvdddt$v)@a$!_cl4%='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []  # '127.0.0.1'
 
